6258ceb4fb3d0bed628d7d3edee64bd8d8a54d5a
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 10:35:14 +0300

    Initial commit

---
+++ FreezingTime/Program.cs
@@ -0,0 +1,12 @@
+﻿class FreezingTime
+{
+    public static void Main(string[] args) {
+        
+    int number=int.Parse(Console.ReadLine());
+
+            if (number<=0) 
+                Console.WriteLine("Freezing weather!");
+
+            
+    }
+    }
